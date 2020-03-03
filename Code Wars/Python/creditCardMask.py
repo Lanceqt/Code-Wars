c205097851d6342c0b5974aedab84c480b42fa88
syntaxsmurf <osdonline@gmail.com>
2020-03-03 21:26:35 +0100

    Code Wars

---
+++ Code Wars/Python/creditCardMask.py
@@ -0,0 +1,28 @@
+'''
+Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, 
+you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+maskify("4556364607935616") == "############5616"
+maskify(     "64607935616") ==      "#######5616"
+maskify(               "1") ==                "1"
+maskify(                "") ==                 ""
+
+# "What was the name of your first pet?"
+maskify("Skippy")                                   == "##ippy"
+maskify("Nananananananananananananananana Batman!") == "####################################man!"
+'''
+
+# return masked string
+def maskify(cc):
+    var = cc
+    l4 = var[-4:]
+    rest = var[:-4]
+    lRest = len(rest)
+    rest = "#" * lRest
+    return rest + l4
+
+var = "Nananananananananananananananana Batman!"
+var = maskify(var)
+print(var)
